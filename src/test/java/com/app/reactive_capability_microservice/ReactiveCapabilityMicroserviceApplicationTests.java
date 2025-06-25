b44f386fe5f661ecc7e14465b6494356a2740a97
Franciscod3v <franciscod3v@gmail.com>
2025-06-25 18:23:22 -0500

    Initial archetype

---
+++ src/test/java/com/app/reactive_capability_microservice/ReactiveCapabilityMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.reactive_capability_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCapabilityMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
