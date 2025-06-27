6df07ad9f0f2d75c382b4286acd5bbd41683813b
Franciscod3v <franciscod3v@gmail.com>
2025-06-26 19:07:34 -0500

    Add TechnologyModel

---
+++ src/main/java/com/app/reactive_capability_microservice/domain/model/Technology.java
@@ -0,0 +1,13 @@
+package com.app.reactive_capability_microservice.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Technology {
+    private Long id;
+    private String name;
+}
